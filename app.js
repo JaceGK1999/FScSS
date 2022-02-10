91bb949b9208640256fa52a9175409b874f440ed
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-02-10 14:38:55 -0600

    adding carousel

--- app.js
+++ app.js
@@ -3,6 +3,26 @@
 // let state
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// get user input
+// use user input to update state 
+// update DOM to reflect the new state
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+plusSlides(n);
+
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName('mySlides');
+    if (n > slides.length) { slideIndex === 1; }
+    if (n < 1) { slideIndex === slides.length; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'block';
+}
